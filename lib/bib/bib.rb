2d4d64aa744dac691cfffcec384c140911d5a4b4
alu0100779622 <alu0100779622@ull.edu.es>
2015-12-17 00:52:46 +0000

    APA

--- lib/bib/bib.rb
+++ lib/bib/bib.rb
@@ -2,58 +2,140 @@ module Bib
 	
   	class Ref
 		include Comparable
-		attr_accessor :autores, :titulo, :serie, :editorial, :numEdicion, :fecha, :numISBN
+		attr_reader :autores, :titulo, :fecha
 		
-		def initialize(a,b,c,d,e,f,g)
-			@autores = a
-			@titulo = b
-			@serie = c 
-			@editorial = d
-			@numEdicion = e
-			@fecha = f
-			@numISBN = g
-		end
-
-		def to_s
-			"#{autores.join(", ")}.\n#{titulo}\n(#{serie})\n#{editorial}; #{numEdicion} edicion #{fecha}\n#{numISBN.join("\n")}"
+		def initialize(aut,title,date)
+			str=""
+			aut.each do |x|
+				separado = x.split(/\W+/)
+				str+=separado[1]
+				str+=", "
+				unless separado[2].nil?
+					str+=separado[2][0]
+					str+=". "
+				end
+				str+=separado[0][0]
+				str+="."
+				str+=" & " unless x == aut.last
+			end
+			@autores = str
+			@titulo = title
+			@fecha = date
 		end
 		
 		def <=>(other)
-			if(@autores!=other.autores)
-				@autores <=> other.autores
+			if(@autores == other.autores)
+				if(@fecha == other.fecha)
+					if(@titulo == other.titulo)
+						return 0 
+					else
+						v = [@titulo, other.titulo]
+						v.sort_by!{|t| t.downcase}
+						if(v.first == @titulo)
+							return 1
+						end
+						return -1
+					end
+				elsif fecha > other.fecha
+					return 1
+				else
+					return -1
+				end
 			else
-				@fecha <=> other.fecha
+				v = [@autores, other.autores]
+				v.sort_by!{|t| t.downcase}
+				if(v.first == @autores)
+					return 1
+				end
+				return -1
 			end
 		end
+		
 	end
 	
 	class Journal < Ref
-		def initialize(a,b,c,d,e,f,g)
-			super(a,b,c,d,e,f,g)
+		def initialize(aut,title,date)
+			super(aut,title,date)
 		end
 	end
 	
 	class Book < Ref
-		def initialize(a,b,c,d,e,f,g)
-			super(a,b,c,d,e,f,g)
+		attr_reader :volumen, :site, :editorial, :edicion
+		def initialize(aut,title,date,editorial,edicion,vol,site)
+			super(aut,title,date)
+			@volumen = vol
+			if site != nil
+				site+=": "
+			end
+			@site = site
+			@editorial = editorial
+			@edicion = edicion
+				
+		end
+		
+		def to_s
+			"#{autores} (#{fecha}). #{titulo} (#{edicion})(#{volumen}). #{site}#{editorial} "
 		end
 	end
 	
 	class Magazine < Journal
-		def initialize(a,b,c,d,e,f,g)
-			super(a,b,c,d,e,f,g)
+		attr_reader :editorial, :edicion, :vol, :site, :capitulo, :editores, :numPag
+		def initialize(aut,title,date,editorial,edicion,vol,site,capitulo,editores,numPag)
+			super(aut,title,date)
+			str=""
+			editores.each do |x|
+				space = x.split(' ')
+				str+=space[1]
+				str+=", "
+				unless space[2].nil?
+					str+=space[2][0]
+					str+=". "
+				end
+				str+=space[0][0]
+				str+="."
+				str+=" & " unless x == editores.last
+			end
+			@editores = str
+			@editorial = editorial
+			@edicion = edicion
+			@vol = vol
+			@site = site
+			@capitulo = capitulo
+			@numPag = numPag
+		end
+		
+		def to_s
+			"#{autores} (#{fecha}). #{capitulo}. #{editores}, #{titulo} (#{numPag}) (#{edicion}) (#{vol}). #{site}: #{editorial}"
 		end
 	end
 	
 	class Newspaper < Journal
-		def initialize(a,b,c,d,e,f,g)
-			super(a,b,c,d,e,f,g)
+		attr_reader :paper, :pag
+		def initialize(aut,title,date,paper,pag)
+			super(aut,title,date)
+			@paper = paper
+			@pag = pag
+		end
+		
+		def to_s
+			"#{autores} (#{fecha}). #{titulo}. #{paper}, pp. #{pag}"	
 		end
 	end
 	
 	class Digital < Journal
-		def initialize(a,b,c,d,e,f,g)
-			super(a,b,c,d,e,f,g)
+		attr_reader :formato, :site, :edicion, :url, :editor, :fechaAcc
+		def initialize(aut,title,date,formato,site,edicion,url,editor,fechaAcc)
+			super(aut,title,date)
+			@formato = formato
+			@site = site
+			@edicion = edicion
+			@url = url
+			@editor = editor
+			@fechaAcc = fechaAcc
+		end
+		
+		def to_s
+			"#{autores} (#{fecha}). #{titulo} (#{edicion}), #{formato}. #{site}: #{editor}. Disponible en: #{url} (#{fechaAcc})"
 		end
 	end
 	
@@ -62,8 +144,8 @@ module Bib
     	Node = Struct.new(:value, :prev, :next)
 		attr_accessor :head, :tail
 		
-		def initialize(ref)
-			@head = Node.new(ref, nil, nil)
+		def initialize()
+			@head = nil
 			@tail = nil
 		end
 		
@@ -113,7 +195,54 @@ module Bib
 				end
 			end
 		end
+		
+		def sort
+			if @tail != nil
+				change = true
+				while change
+					change = false
+					item = @head
+					next_item = @head[:next]
+					while next_item != nil
+						if(item.value > next_item.value)
+							item[:value], next_item[:value] = next_item[:value], item[:value]
+							change = true
+						end
+						item = next_item
+						next_item = next_item[:next]
+					end
+				end
+			else
+				return
+			end
+		end
+	end
+	
+	class RefList
+		
+		include Enumerable
+		
+		def initialize()
+			@RefList = Bib::List.new
+		end
+		
+		def insert(ref)
+			@RefList.add(ref)
+			@RefList.sort
+		end
+		
+		def each
+			@RefList.each{ |x| yield x}
+		end
+		def to_s
+			str=""
+			@RefList.each do |x|
+				str << x.to_s
+				str << "\n"
+			end
+			return str
+		end
 	end
 end
 
-
+	
--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -5,33 +5,27 @@ describe Bib do
   before :each do 
 		@Lib1 = Bib::Book.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],
 			"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide",
-			"The Facets of Ruby",
-			"Pragmatic Bookshelf",4,"July 7,2013",
-			["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
-		@Lib2 = Bib::Book.new("Scott Chacon", 
+			"July 7,2013","Pragmatic Bookshelf",
+			4,1,"EEUU")
+		@Lib2 = Bib::Book.new(["Scott Chacon"], 
 			"Pro Git 2009th Edition",
-			"Pro",
-			"Apress","2009 edition","August 27,2009",
-            ["ISBN-13: 978-1430218333","ISBN-10: 1430218339"])
+			"August 27,2009","Apress","2009 edition",1,
+			nil)
         @Lib3 = Bib::Book.new(["David Flanagan", "Yukihiro Matsumoto"],
-        	"The Ruby Programming Language",
-        	nil,
-            "O'Reilly Media","1 edition","February 4,2008",
-            ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
+        	"February 4,2008","The Ruby Programming Language",
+            "O'Reilly Media","1 edition",1,nil)
         @Lib4 = Bib::Book.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"],
             "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends (The Facets of Ruby)",
-            nil,
-            "Pragmatic Bookshelf","1 edition","December 25,2010",
-            ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
-        @Lib5 = Bib::Book.new("Richard E. Silverman",
+            "December 25,2010","Pragmatic Bookshelf","1 edition",1,
+            nil)
+        @Lib5 = Bib::Book.new(["Richard E. Silverman"],
         	"Git Pocket Guide",
-        	nil,
-        	"O'Reilly Media","1 edition","August 2,2013",
-            ["ISBN-10: 1449325866","ISBN-13: 978-1449325862"])
+        	"August 2,2013","O'Reilly Media","1 edition",1,
+            nil)
             
-        @List = Bib::List.new @Lib1
-        @ListVoid = Bib::List.new nil
-        
+        @List = Bib::List.new
+        @List.add @Lib1
+        @ListVoid = Bib::List.new
 	end
 
 	describe "Expectativas de los atributos" do 
@@ -42,20 +36,8 @@ describe Bib do
 		it "Debe existir un titulo" do
 			expect(@Lib1.titulo.length).not_to be 0
 		end
-		it "Debe existir o no una serie" do
-			expect(@Lib1.serie.length).to be >=0
-		end
-		it "Debe existir una editorial" do
-			expect(@Lib1.editorial.length).not_to be 0
-		end
-		it "Debe existir un numero de edicion" do
-			expect(@Lib1.numEdicion.to_s.length).not_to be 0
-		end
 		it "Debe existir una fecha de publicacion" do
 			expect(@Lib1.fecha.length).not_to be 0
-		end
-		it "Debe existir uno o mas numeros ISBN." do
-			expect(@Lib1.numISBN).not_to be_empty
 		end
 	end
 	
@@ -67,23 +49,12 @@ describe Bib do
 		it "Existe un metodo para obtener el titulo." do
 			@Lib1.titulo =="Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide"
 		end
-		it "Existe un metodo para obtener la serie" do
-			@Lib1.serie =="The Facets of Ruby"
-		end
-		it "Existe un metodo para obtener la editorial." do
-			@Lib1.editorial =="Pragmatic Bookshelf"
-		end
-		it "Existe un metodo para obtener el numero de edicion" do
-			@Lib1.numEdicion == 4
-		end
+		
 		it "Existe un metodo para obtener la fecha de publicacion." do
 			@Lib1.fecha =="July 7,2013"
 		end
-		it "Existe un metodo para obtener el listado de ISBN" do
-			@Lib1.numISBN == ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"]
-		end
 		it "Existe un metodo para obtener la referencia formateada" do
-		  expect(@Lib1.to_s).to eq("Dave Thomas, Andy Hunt, Chad Fowler.\nProgramming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide\n(The Facets of Ruby)\nPragmatic Bookshelf; 4 edicion July 7,2013\nISBN-13: 978-1937785499\nISBN-10: 1937785491")
+		  expect(@Lib1.to_s).to eq("Thomas, D. & Hunt, A. & Fowler, C. (July 7,2013). Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide (4)(1). EEUU: Pragmatic Bookshelf ")
 	    end
 	end
 	
@@ -142,32 +113,25 @@ describe Bib do
 	
 	describe "Expectativas Comparable" do
 		before :each do
-			@Book1 = Bib::Book.new("Pepe",
-			"Libro1",
-			nil,
-			"editorial",nil,1998,
+			@Book1 = Bib::Book.new(["Pepe Ter"],
+			"ABC",
+			1998,"editorial",nil,nil,
 			nil)
-			@Book2 = Bib::Book.new("Pablo",
-			"Libro2",
-			nil,
-			"editorial",nil,2000,
+			@Book2 = Bib::Book.new(["Pepe Ter"],
+			"BCD",
+			2000,"editorial",nil,nil,
 			nil)
-			@Book3 = Bib::Book.new("Pepe",
-			"Libro3",
-			nil,
-			"editorial",nil,2004,
+			@Book3 = Bib::Book.new(["Pepe Ter"],
+			"CDE",
+			1998,"editorial",nil,nil,
 			nil)
 		end
 		
-		it "Comparacion entre dos referencias" do
-			expect(@Lib1 == @Lib1).to eq(true)
-			expect(@Lib1 == @Lib2).to eq(false)
-		end
 		it "Ref1 < Ref2" do
-			expect(@Book1 < @Book3).to eq(true)
+			expect(@Book1 > @Book3).to eq(true)
 		end
 		it "Ref1 >= Ref2" do
-			expect(@Book1 >= @Book3).to eq(false)
+			expect(@Book1 <= @Book3).to eq(false)
 		end
 	end
 	
@@ -186,4 +150,27 @@ describe Bib do
       		expect(@List.count).to eq(2)
     	end
 	end
+	describe "(APA)" do
+		before :all do
+			@Bibliografia = Bib::RefList.new
+			@Libro = Bib::Book.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],
+			"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide",
+			"July 7,2013","Pragmatic Bookshelf",
+			4,1,"EEUU")
+			@Articulo = Bib::Magazine.new(["Carlos Area","Pedro Gimenez"],"El universo: Agujeros negros",2010,"Trompeta",7,2,"Argentina","Astrofísica",["Eduardo Morales","Jorge Castilla"],12)
+			@Periodico = Bib::Newspaper.new(["Maria Gonzales","Omaira Alvarez"],"Politica del siglo XXI","Agosto 2012","Publico",2)
+			@Web = Bib::Digital.new(["Alberto Cejas"],"Cambio climatico","2 Diciembre 1999","Blog","España",2,"www.blogdealberto.com","ONU","3 Enero 2004")
+		end
+		it "Insertar elementos, y ordenar automaticamnete" do
+			@Bibliografia.insert(@Libro)
+			@Bibliografia.insert(@Articulo)
+			@Bibliografia.insert(@Periodico)
+			@Bibliografia.insert(@Web)
+			expect(@Bibliografia.to_s).to eq("Thomas, D. & Hunt, A. & Fowler, C. (July 7,2013). Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide (4)(1). EEUU: Pragmatic Bookshelf \nGonzales, M. & Alvarez, O. (Agosto 2012). Politica del siglo XXI. Publico, pp. 2\nCejas, A. (2 Diciembre 1999). Cambio climatico (2), Blog. España: ONU. Disponible en: www.blogdealberto.com (3 Enero 2004)\nArea, C. & Gimenez, P. (2010). Astrofísica. Morales, E. & Castilla, J., El universo: Agujeros negros (12) (7) (2). Argentina: Trompeta\n")
+		end
+	end
 end
+
+
+
+
